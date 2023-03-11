4486bc3191cd02859c77583902f086247c6f6036
Erik Bernskiold <erik@bernskioldmedia.com>
2023-03-11 09:44:26 +0100

    Support language code
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- src/Commands/TranslateFile.php
+++ src/Commands/TranslateFile.php
@@ -3,29 +3,30 @@
 namespace BernskioldMedia\Autotranslate\Commands;
 
 use BernskioldMedia\Autotranslate\TranslateStrings;
-use function collect;
 use DeepL\DeepLException;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
-use function json_decode;
-use const JSON_THROW_ON_ERROR;
 use JsonException;
+use function collect;
+use function json_decode;
 use function lang_path;
+use const JSON_THROW_ON_ERROR;
 
 class TranslateFile extends Command
 {
-    public $signature = 'autotranslate:translate {lang}';
+    public $signature = 'autotranslate:translate {lang} {deeplCode?}';
 
     public $description = 'Translates the selected language file into the selected language.';
 
     public function handle(TranslateStrings $translator): int
     {
         $language = $this->argument('lang');
+        $deeplLanguageCode = $this->argument('deeplCode', $language);
 
-        $path = lang_path($language.'.json');
+        $path = lang_path($language . '.json');
         $errors = [];
 
-        $this->comment('Translating the file: '.$path);
+        $this->comment('Translating the file: ' . $path);
 
         try {
             $contents = File::get($path);
@@ -33,10 +34,10 @@ class TranslateFile extends Command
 
             $translations = collect($originals)
                 ->chunk(50)
-                ->map(function ($chunk) use ($translator, $language, &$errors) {
+                ->map(function ($chunk) use ($translator, $deeplLanguageCode, &$errors) {
                     try {
-                        return $translator->execute($chunk, $language)
-                            ->map(fn ($value, $key) => [
+                        return $translator->execute($chunk, $deeplLanguageCode)
+                            ->map(fn($value, $key) => [
                                 'original' => $key,
                                 'translation' => $value,
                             ]);
@@ -56,7 +57,7 @@ class TranslateFile extends Command
             return self::FAILURE;
         }
 
-        if (! empty($errors)) {
+        if (!empty($errors)) {
             $this->error('The following errors occurred:');
             foreach ($errors as $error) {
                 $this->error($error);
